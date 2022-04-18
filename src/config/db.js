477a64a172fae79be67def9b4ad97863c511f75a
Anushka <95957335+anushka3002@users.noreply.github.com>
2022-04-18 23:14:17 +0530

    updated

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose")
-const connect = () =>{
-    return mongoose.connect(
-        "mongodb+srv://anushka:anushka_123@cluster0.9mnv1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-    )
-}
 
-module.exports = connect
+
+module.exports =()=>{
+    return mongoose.connect(
+        process.env.MONGO_URL,
+        {useNewUrlParser:true}
+    )
+} 
